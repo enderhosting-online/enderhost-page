d1a2f93f07a8e9d016968e4de528aed7052bfc6d
ynoacamino <ynoacamino@gmail.com>
2025-07-06 13:49:34 -0500

    add metadata

---
+++ src/config/metadata.ts
@@ -0,0 +1,45 @@
+import { Metadata } from 'next';
+
+export const metadataConfig: Metadata = {
+  title: 'YN HOSTING',
+  description: 'Hostea tu servidor de Minecraft con nuestra plataforma fácil de usar y rápida. No pierdas más tiempo, ¡comienza a jugar ahora!',
+  keywords: [
+    'YN HOSTING',
+    'Minecraft Hosting',
+    'Servidor de Minecraft',
+    'Hosting de Juegos',
+    'Servidor de Juegos',
+    'Hosting Rápido',
+    'Fácil de Usar',
+    'Comienza a Jugar',
+  ],
+  openGraph: {
+    title: 'YN HOSTING',
+    description: 'Hostea tu servidor de Minecraft con nuestra plataforma fácil de usar y rápida. No pierdas más tiempo, ¡comienza a jugar ahora!',
+    url: 'https://hosting.ynoacamino.com',
+    siteName: 'YN HOSTING',
+    images: [
+      {
+        url: 'https://ynoa-uploader.ynoacamino.site/uploads/1751827627_2025-07-06_13-46.webp',
+        width: 700,
+        height: 371,
+        alt: 'YN HOSTING Open Graph Image',
+      },
+    ],
+    locale: 'es',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'YN HOSTING',
+    description: 'Hostea tu servidor de Minecraft con nuestra plataforma fácil de usar y rápida. No pierdas más tiempo, ¡comienza a jugar ahora!',
+    images: [
+      {
+        url: 'https://ynoa-uploader.ynoacamino.site/uploads/1751827627_2025-07-06_13-46.webp',
+        width: 700,
+        height: 371,
+        alt: 'YN HOSTING Twitter Image',
+      },
+    ],
+  },
+};
