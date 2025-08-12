0d934bcd94702e23c4414abaa951c419fad702b3
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2025-08-12 14:54:16 -0500

    feat: add million library to optimized

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from 'next';
+import MillionLint from '@million/lint';
 
-const nextConfig: NextConfig = {
+const baseConfig: NextConfig = {
   output: 'standalone',
+  reactStrictMode: false,
 };
+
+const nextConfig: NextConfig = process.env.NODE_ENV === 'production'
+  ? MillionLint.next({ rsc: true })(baseConfig)
+  : baseConfig;
 
 export default nextConfig;
