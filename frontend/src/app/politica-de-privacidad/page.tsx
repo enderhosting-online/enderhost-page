5e642d3d70980602e84fbf07803f5f376de25d50
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2025-08-17 20:38:08 -0500

    feat: add enviroment variable

--- frontend/src/app/politica-de-privacidad/page.tsx
+++ frontend/src/app/politica-de-privacidad/page.tsx
@@ -4,6 +4,6 @@ export default async function PrivacyPolicy() {
   const privacyPolicy = await apiService.getPrivacyPolicy();
 
   return (
-    <div className="prose text-foreground prose-strong:text-foreground prose-headings:text-foreground w-full md:prose-lg my-14 max-w-6xl px-6" dangerouslySetInnerHTML={{ __html: privacyPolicy.content }} />
+    <div className="prose text-foreground prose-strong:text-foreground prose-headings:text-foreground w-full md:prose-lg my-20 max-w-6xl px-6 prose-a:text-foreground" dangerouslySetInnerHTML={{ __html: privacyPolicy.content }} />
   );
 }
