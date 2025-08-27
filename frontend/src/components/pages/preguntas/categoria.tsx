64d8762953a8fd065670f088316b2ec9046aaf49
Yenaro Joel Noa Camino <ynoacamino@gmail.com>
2025-08-27 15:59:12 -0500

    feat: add maosry layout in preguntas page

--- frontend/src/components/pages/preguntas/categoria.tsx
+++ frontend/src/components/pages/preguntas/categoria.tsx
@@ -6,11 +6,16 @@ import { sf } from '@/lib/utils';
 import { Question } from '@/types/directus';
 
 import { motion } from 'motion/react';
+import { useEffect } from 'react';
 
 export default function Categoria(
   { category, questions, id }:
   { category: string, questions: Question[], id: string },
 ) {
+  useEffect(() => {
+    import('@appnest/masonry-layout');
+  }, []);
+
   return (
     <Section
       className="flex flex-col gap-16 my-20"
@@ -19,23 +24,23 @@ export default function Categoria(
       <Title className="max-w-lg">
         {category}
       </Title>
-      <div className="grid grid-cols-2 gap-8">
+      <masonry-layout maxcolwidth="700" className="md:gap-2">
         {
           questions.map(({ answer, title }) => (
             <motion.div
               key={title}
-              className="p-6 gap-8 rounded-xl border-2 border-[#24282F] bg-[#171B22] text-xl flex flex-col"
+              className="p-6 gap-8 rounded-xl border-2 border-[#24282F] bg-[#171B22] text-xl flex flex-col max-h-max mb-6 md:mb-8 w-full"
               whileHover={{ borderColor: '#616671' }}
               id={sf(title)}
             >
               <span className="w-full text-2xl font-semibold">
                 {title}
               </span>
-              <div className="text-base flex flex-col gap-6" dangerouslySetInnerHTML={{ __html: answer }} />
+              <div className="text-base flex flex-col gap-6 prose-img:rounded-lg prose-img:my-2" dangerouslySetInnerHTML={{ __html: answer }} />
             </motion.div>
           ))
         }
-      </div>
+      </masonry-layout>
     </Section>
   );
 }
